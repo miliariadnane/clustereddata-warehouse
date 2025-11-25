a9065407a1e8daeac9aba0c097b8e67b0f876be2
Adnane Miliari <adnane.miliari@transperfect.com>
2025-11-25 08:50:03 +0100

    Replace `@MockBean` with `@MockitoBean` in `DealControllerTest`.

--- src/test/java/com/bloomberg/clustereddata/controller/DealControllerTest.java
+++ src/test/java/com/bloomberg/clustereddata/controller/DealControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,7 +32,7 @@ class DealControllerTest {
 
     @Autowired private ObjectMapper objectMapper;
 
-    @MockBean private DealService dealService;
+    @MockitoBean private DealService dealService;
 
     @Test
     void createDeal_shouldReturnCreated() throws Exception {
